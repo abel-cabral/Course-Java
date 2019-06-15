d4678a3e8d42df80e8c525d46a20784be21e535e
Abel Warakia <jcabral@id.uff.br>
2019-06-15 15:08:10 -0300

    FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("dfdf");
+		// Linux Open File
+		File homedir = new File(System.getProperty("user.home"));
+		File path = new File(homedir, "qualquer.txt");
+
+		// Declara variavel no try
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			// Java 7 > o fechamento é automatico no bloco try/cath
+			System.out.println("Error: " + e.getMessage());
+		}
+
 	}
 
 }
