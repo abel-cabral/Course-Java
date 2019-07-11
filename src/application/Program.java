1a65ac42372ffc155691c77da7b3015fdb0c9560
warakia <jcabral@id.uff.br>
2019-07-11 13:21:06 -0300

    finalizacao exercicio

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,59 +3,50 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import entities.Enade;
+import entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String path = "/Users/warakia/Projetos/Java/Course-Java/enade.csv";
+		String path = "/Users/warakia/Projetos/Java/Course-Java/funcionarios.csv";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			List<Enade> list = new ArrayList<Enade>();
+			List<Employee> list = new ArrayList<Employee>();
+
+			System.out.printf("Enter salary: ");
+			Double minSalary = sc.nextDouble();
+
+			System.out.printf("Email of people whose salary is more than %.2f\n", minSalary);
 
 			String line = br.readLine();
-			line = br.readLine();
 			while (line != null) {
 				try {
-					String[] aux = line.split(";");
-					Integer codeCourse = Integer.parseInt(aux[1]);
-					String nameCourse = aux[2];
-					String university = aux[4];
-					String shortUniversity = aux[5];
-					Integer concept = Integer.parseInt(aux[20]);
-					list.add(new Enade(codeCourse, university, shortUniversity, concept, nameCourse));
+					String[] aux = line.split(",");
+					String name = aux[0];
+					String email = aux[1];
+					Double salary = Double.parseDouble(aux[2]);
+
+					list.add(new Employee(name, email, salary));
 				} catch (Exception e) {
-					// Caso queira tratar os curso inválidos
+					// Caso queira tratar os inválidos
 				}
 				line = br.readLine();
 			}
 
-			// Verifica as maiores notas
-			Comparator<Enade> comp = (s1, s2) -> s1.getConcept().compareTo(s2.getConcept());
+			List<String> employeeEmail = list.stream().filter(p -> p.getSalary() >= minSalary).map(p -> p.getEmail())
+					.sorted().collect(Collectors.toList());
 
-			// Filtra cursos e faz média
-			List<Enade> course = list.stream().filter(p -> p.getCodeCourse() == 4006).collect(Collectors.toList());
-			Integer media = course.stream().map(n -> n.getConcept()).reduce(0,
-					(subtotal, element) -> subtotal + element);
+			employeeEmail.forEach(System.out::println);
 
-			System.out.println(Enade.mediaNacional(course.get(0).getNameCourse(),
-					(int) Math.ceil(media / (double) course.size())));
-
-			// Filtra e ordena pelas maiores notas
-			System.out.println("Cursos Acima da Média: " + (int) Math.ceil(media / (double) course.size()));
-			List<Enade> topCourse = course.stream()
-					.filter(p -> p.getConcept() > (int) Math.ceil(media / (double) course.size()))
-					.sorted(comp.reversed()).collect(Collectors.toList());
-
-			for (Enade s : topCourse) {
-				System.out.println(s.toString());
-			}
+			double media = list.stream().filter(x -> x.getName().charAt(0) == 'M').map(n -> n.getSalary()).reduce(0.0,
+					(identificator, accumulator) -> identificator + accumulator);
+			
+			System.out.println("Sum of salary from people whose name starts with 'M': " + String.format("%.2f", media));
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
